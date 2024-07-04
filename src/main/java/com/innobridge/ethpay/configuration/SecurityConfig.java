44274741180920f29e31594f12a0aac08258bbc6
Yi leng Yao <yilengyao@gmail.com>
2024-07-03 21:12:07 -0700

    securityfilterchain with basic auth and require auth on all requests

--- src/main/java/com/innobridge/ethpay/configuration/SecurityConfig.java
+++ src/main/java/com/innobridge/ethpay/configuration/SecurityConfig.java
@@ -6,12 +6,17 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
+import static org.springframework.security.config.Customizer.withDefaults;
+
 @EnableWebSecurity
 @Configuration
 public class SecurityConfig {
 
   @Bean
   SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+      http.httpBasic(withDefaults());
+      http.authorizeHttpRequests((requests)
+          -> requests.anyRequest().authenticated());
       return http.build();
   }
 
