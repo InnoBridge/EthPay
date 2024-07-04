b417b1832b893a04e878182373bd1dfd6014aa52
Yi leng Yao <yilengyao@gmail.com>
2024-07-03 20:58:36 -0700

    minimum securityfilterchain

--- src/main/java/com/innobridge/ethpay/configuration/SecurityConfig.java
+++ src/main/java/com/innobridge/ethpay/configuration/SecurityConfig.java
@@ -2,25 +2,28 @@ package com.innobridge.ethpay.configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 @EnableWebSecurity
 @Configuration
 public class SecurityConfig {
 
   @Bean
-  UserDetailsService userDetailsService() {
-    UserDetails user = User.withUsername("user")
-        .password("{noop}password")
-        .authorities("read")
-        .roles("USER")
-        .build();
-
-    return new InMemoryUserDetailsManager(user);
+  SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+      return http.build();
   }
+
+  // @Bean
+  // UserDetailsService userDetailsService() {
+  //   UserDetails user = User.withUsername("user")
+  //       .password("{noop}password")
+  //       .authorities("read")
+  //       .roles("USER")
+  //       .build();
+  //
+  //   return new InMemoryUserDetailsManager(user);
+  // }
 
 }
