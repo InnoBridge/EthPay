6de306bdf6828b3aaa6d666a7ecf7e91acdcbef6
Yi leng Yao <yilengyao@gmail.com>
2024-08-19 16:22:28 -0700

    remove jsession id, revert the requests to stateless afther the oauth key exchange flow

--- src/main/java/com/innobridge/ethpay/controller/OAuthController.java
+++ src/main/java/com/innobridge/ethpay/controller/OAuthController.java
@@ -1,6 +1,7 @@
 package com.innobridge.ethpay.controller;
 
 import com.innobridge.ethpay.model.AccessTokenResponse;
+import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,6 +15,11 @@ public class OAuthController {
 
     @GetMapping("/oauth2/success")
     public ResponseEntity<?> success(@RequestParam String accessToken, HttpServletResponse response) {
+        // Remove JSESSIONID cookie
+        Cookie jsessionidCookie = new Cookie("JSESSIONID", null);
+        jsessionidCookie.setPath("/");
+        jsessionidCookie.setMaxAge(0);
+        response.addCookie(jsessionidCookie);
 
         return ResponseEntity.ok(new AccessTokenResponse(accessToken, ACCESS_TOKEN_EXPIRATION_TIME.toSeconds()));
     }
